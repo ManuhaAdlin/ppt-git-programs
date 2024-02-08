6fcef25510fbf631730c623596ef0f3f03e53fe3
ManuhaAdlin <adlinmanuha2316@gmail.com>
2024-02-08 11:32:26 +0530

    first

---
+++ Checkoddarray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Checkoddarray {
+    public Checkoddarray() {
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter the array");
+
+        int i;
+        for(i = 0; i < array.length; ++i) {
+            array[i] = s1.nextInt();
+        }
+
+        for(i = 0; i < array.length; ++i) {
+            if (array[i] % 2 != 0) {
+            }
+        }
+
+        System.out.println("Enter array is odd");
+    }
+}
